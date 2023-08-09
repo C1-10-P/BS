56c74dd6541a5a33f6845e4d5c9e1f6b6ab609c5
dadBod-Kui <98661216+dadBod-Kui@users.noreply.github.com>
2023-08-09 17:27:47 +0300

    testing home

--- index.php
+++ index.php
@@ -1,81 +1,325 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include('includes/partials/header.php')?>
-<link href="css/home_1.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Ansonika">
+    <title>THE READS
+		
+	</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+	
+    <!-- GOOGLE WEB FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+	<!-- SPECIFIC CSS -->
+    <link href="css/home_1.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
 
 <body>
 	
 	<div id="page">
 		
 	<header class="version_1">
-	<?php include('includes/nav.php')?>
-		<!-- /main_nav -->
-	</header>
-	<!-- /header -->
-		
+	<div class="layer"></div><!-- Mobile menu overlay mask -->
+		<div class="main_header">
+			<div class="container">
+				<div class="row small-gutters">
+					<div class="col-xl-3 col-lg-3 d-lg-flex align-items-center">
+						<div id="logo">
+							<a href="index.php"><img src="images/logo2.png" alt="" width="100" height="35"></a>
+						</div>
+					</div>
+					<nav class="col-xl-6 col-lg-7">
+						<a class="open_close" href="javascript:void(0);">
+							<div class="hamburger hamburger--spin">
+								<div class="hamburger-box">
+									<div class="hamburger-inner"></div>
+								</div>
+							</div>
+						</a>
+						<!-- Mobile menu button -->
+						<div class="main-menu">
+							<div id="header_menu">
+								<a href="index.php"><img src="img/logo_black.svg" alt="" width="100" height="35"></a>
+								<a href="#" class="open_close" id="close_in"><i class="ti-close"></i></a>
+							</div>
+							<ul>
+								<li class="submenu">
+									<a href="javascript:void(0);" class="show-submenu">Home</a>
+									<ul>
+										<li><a href="index.php">Slider</a></li>
+										
+										
+									</ul>
+								</li>
+								<li class="megamenu submenu">
+									<a href="javascript:void(0);" class="show-submenu-mega">Pages</a>
+									<div class="menu-wrapper">
+										<div class="row small-gutters">
+											<div class="col-lg-3">
+												<h3>Listing grid</h3>
+												<ul>
+													<li><a href="listing-grid-1-full.php">Grid Full Width</a></li>
+													<li><a href="listing-grid-1-full.php">Grid Full Width 2</a></li>
+													
+													
+													<li><a href="listing-grid-7-sidebar-right.php">Grid Sidebar Right</a></li>
+													
+													<li><a href="listing-grid-7-sidebar-right.php">Grid Sidebar Right 2</a></li>
+												</ul>
+											</div>
+											<div class="col-lg-3">
+												<h3>Listing row &amp; Product</h3>
+												<ul>
+													<li><a href="listing-row-3-sidebar-left.php">Row Sidebar Left</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">Row Sidebar Right</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">Row Sidebar Left 2</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">Row Sidebar Extended</a></li>
+													<li><a href="product-detail-1.php">Product Large Image</a></li>
+													<li><a href="product-detail-2.php">Product Carousel</a></li>
+													
+												</ul>
+											</div>
+											<div class="col-lg-3">
+												<h3>Other pages</h3>
+												<ul>
+													<li><a href="cart.php">Cart Page</a></li>
+													<li><a href="checkout.php">Check Out Page</a></li>
+													<li><a href="confirm.php">Confirm Purchase Page</a></li>
+													<li><a href="account.php">Create Account Page</a></li>
+													<li><a href="track-order.php">Track Order</a></li>
+													<li><a href="help.php">Help Page</a></li>
+													
+													<li><a href="leave-review.php">Leave a Review</a></li>
+												</ul>
+											</div>
+											<div class="col-lg-3 d-xl-block d-lg-block d-md-none d-sm-none d-none">
+												<div class="banner_menu">
+													<a href="#0">
+														<img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/banner_menu.jpg" width="400" height="550" alt="" class="img-fluid lazy">
+													</a>
+												</div>
+											</div>
+										</div>
+										<!-- /row -->
+									</div>
+									<!-- /menu-wrapper -->
+								</li>
+								<li class="submenu">
+									<a href="javascript:void(0);" class="show-submenu">Extra Pages</a>
+									<ul>
+										
+										<li><a href="404.php">404 Page</a></li>
+										<li><a href="sign-in-modal.php">Sign In Modal</a></li>
+										<li><a href="contacts.php">Contact Us</a></li>
+										<li><a href="about.php">About </a></li>
+										
+										<li><a href="modal-advertise.php">Modal Advertise</a></li>
+										<li><a href="modal-newsletter.php">Modal Newsletter</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="blog.php">Blog</a>
+								</li>
+								<li>
+									<a href="about.php">About</a>
+								</li>
+							</ul>
+						</div>
+						<!--/main-menu -->
+					</nav>
+					<div class="col-xl-3 col-lg-2 d-lg-flex align-items-center justify-content-end text-end">
+						<a class="phone_top" href="tel://9438843343"><strong><span>Need Help?</span>+254 740567299</strong></a>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+		</div>
+		<!-- /main_header -->
+
+		<div class="main_nav inner Sticky">
+			<div class="container">
+				<div class="row small-gutters">
+					<div class="col-xl-3 col-lg-3 col-md-3">
+						<nav class="categories">
+							<ul class="clearfix">
+								<li><span>
+										<a href="#">
+											<span class="hamburger hamburger--spin">
+												<span class="hamburger-box">
+													<span class="hamburger-inner"></span>
+												</span>
+											</span>
+											Categories
+										</a>
+									</span>
+									<div id="menu">
+										<ul>
+											<li><span><a href="#0">Collections</a></span>
+												<ul>
+													<li><a href="listing-grid-1-full.php">Trending</a></li>
+													<li><a href="listing-grid-1-full.php">Life style</a></li>
+													<li><a href="listing-grid-1-full.php">Running</a></li>
+													<li><a href="listing-grid-1-full.php">Training</a></li>
+													<li><a href="listing-grid-1-full.php">View all Collections</a></li>
+												</ul>
+											</li>
+											<li><span><a href="#">Men</a></span>
+												<ul>
+													<li><a href="listing-grid-7-sidebar-right.php">Offers</a></li>
+													<li><a href="listing-grid-7-sidebar-right.php">Shoes</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">Clothing</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">Accessories</a></li>
+													<li><a href="listing-grid-7-sidebar-right.php">Equipment</a></li>
+												</ul>
+											</li>
+											<li><span><a href="#">Women</a></span>
+												<ul>
+													<li><a href="listing-grid-2-full.php">Best Sellers</a></li>
+													<li><a href="listing-grid-2-full.php">Clothing</a></li>
+													<li><a href="listing-grid-2-full.php">Accessories</a></li>
+													<li><a href="listing-grid-2-full.php">Shoes</a></li>
+												</ul>
+											</li>
+											<li><span><a href="#">Boys</a></span>
+												<ul>
+													<li><a href="listing-grid-7-sidebar-right.php">Easy On Shoes</a></li>
+													<li><a href="listing-grid-7-sidebar-right.php">Clothing</a></li>
+													<li><a href="listing-grid-7-sidebar-right.php">Must Have</a></li>
+													<li><a href="listing-grid-7-sidebar-right.php">All Boys</a></li>
+												</ul>
+											</li>
+											<li><span><a href="#">Girls</a></span>
+												<ul>
+													<li><a href="listing-grid-2-full.php">New Releases</a></li>
+													<li><a href="listing-grid-2-full.php">Clothing</a></li>
+													<li><a href="listing-grid-2-full.php">Sale</a></li>
+													<li><a href="listing-grid-2-full.php">Best Sellers</a></li>
+												</ul>
+											</li>
+											<li><span><a href="#">Customize</a></span>
+												<ul>
+													<li><a href="listing-row-3-sidebar-left.php">For Men</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">For Women</a></li>
+													<li><a href="listing-row-3-sidebar-left.php">For Boys</a></li>
+													<li><a href="listing-grid-2-full.php">For Girls</a></li>
+												</ul>
+											</li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div class="col-xl-6 col-lg-7 col-md-6 d-none d-md-block">
+						<div class="custom-search-input">
+							<input type="text" placeholder="Search over 1000 books">
+							<button type="submit"><i class="header-icon_search_custom"></i></button>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-2 col-md-3">
+						<ul class="top_tools">
+							<li>
+								<div class="dropdown dropdown-cart">
+									<a href="cart.php" class="cart_bt"><strong>2</strong></a>
+									<div class="dropdown-menu">
+										<ul>
+											<li>
+												<a href="product-detail-1.php">
+													<figure><img src="img/products/product_placeholder_square_small.jpg" data-src="img/products/shoes/thumb/1.jpg" alt="" width="50" height="50" class="lazy"></figure>
+													<strong><span>1x Armor Air x Fear</span>$90.00</strong>
+												</a>
+												<a href="#0" class="action"><i class="ti-trash"></i></a>
+											</li>
+											<li>
+												<a href="product-detail-1.php">
+													<figure><img src="img/products/product_placeholder_square_small.jpg" data-src="img/products/shoes/thumb/2.jpg" alt="" width="50" height="50" class="lazy"></figure>
+													<strong><span>1x Armor Okwahn II</span>$110.00</strong>
+												</a>
+												<a href="0" class="action"><i class="ti-trash"></i></a>
+											</li>
+										</ul>
+										<div class="total_drop">
+											<div class="clearfix"><strong>Total</strong><span>$200.00</span></div>
+											<a href="cart.php" class="btn_1 outline">View Cart</a><a href="checkout.php" class="btn_1">Checkout</a>
+										</div>
+									</div>
+								</div>
+								<!-- /dropdown-cart-->
+							</li>
+							<li>
+								<a href="wishlist.php" class="wishlist"><span>Wishlist</span></a>
+							</li>
+							<li>
+								<div class="dropdown dropdown-access">
+									<a href="account.php" class="access_link"><span>Account</span></a>
+									<div class="dropdown-menu">
+										<a href="account.php" class="btn_1">Sign In or Sign Up</a>
+										<ul>
+											<li>
+												<a href="track-order.php"><i class="ti-truck"></i>Track your Order</a>
+											</li>
+											<li>
+												<a href="account.php"><i class="ti-package"></i>My Orders</a>
+											</li>
+											<li>
+												<a href="account.php"><i class="ti-user"></i>My Profile</a>
+											</li>
+											<li>
+												<a href="help.php"><i class="ti-help-alt"></i>Help and Faq</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<!-- /dropdown-access-->
+							</li>
+							<li>
+								<a href="javascript:void(0);" class="btn_search_mob"><span>Search</span></a>
+							</li>
+							<li>
+								<a href="#menu" class="btn_cat_mob">
+									<div class="hamburger hamburger--spin" id="hamburger">
+										<div class="hamburger-box">
+											<div class="hamburger-inner"></div>
+										</div>
+									</div>
+									Categories
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<div class="search_mob_wp">
+				<input type="text" class="form-control" placeholder="Search over 1000 books">
+				<input type="submit" class="btn_1 full-width" value="Search">
+			</div>
+			<!-- /search_mobile -->
+		</div>
 		
 	<main>
-		<div id="carousel-home">
-			<div class="owl-carousel owl-theme">
-				<div class="owl-slide cover" style="background-image: url(immages/ban1.jpeg);">
-					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-						<div class="container">
-							<div class="row justify-content-center justify-content-md-end">
-								<div class="col-lg-6 static">
-									<div class="slide-text text-end white">
-										<h2 class="owl-slide-animated owl-slide-title">Attack Air<br>Max 720 Sage Low</h2>
-										<p class="owl-slide-animated owl-slide-subtitle">
-											Limited items available at this price
-										</p>
-										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1" href="listing-grid-1-full.html" role="button">Shop Now</a></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!--/owl-slide-->
-				<div class="owl-slide cover" style="background-image: url(img/slides/slide_home_1.jpg);">
-					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-						<div class="container">
-							<div class="row justify-content-center justify-content-md-start">
-								<div class="col-lg-6 static">
-									<div class="slide-text white">
-										<h2 class="owl-slide-animated owl-slide-title">Attack Air<br>VaporMax Flyknit 3</h2>
-										<p class="owl-slide-animated owl-slide-subtitle">
-											Limited items available at this price
-										</p>
-										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1" href="listing-grid-1-full.html" role="button">Shop Now</a></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!--/owl-slide-->
-				<div class="owl-slide cover" style="background-image: url(img/slides/slide_home_3.jpg);">
-					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(255, 255, 255, 0.5)">
-						<div class="container">
-							<div class="row justify-content-center justify-content-md-start">
-								<div class="col-lg-12 static">
-									<div class="slide-text text-center black">
-										<h2 class="owl-slide-animated owl-slide-title">Attack Air<br>Monarch IV SE</h2>
-										<p class="owl-slide-animated owl-slide-subtitle">
-											Lightweight cushioning and durable support with a Phylon midsole
-										</p>
-										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1" href="listing-grid-1-full.html" role="button">Shop Now</a></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!--/owl-slide-->
-				</div>
-			</div>
-			<div id="icon_drag_mobile"></div>
-		</div>
-		<!--/carousel-->
+		
 
 		<ul id="banners_grid" class="clearfix">
 			<li>
@@ -98,7 +342,7 @@
 			</li>
 			<li>
 				<a href="#0" class="img_container">
-					<img src="images/c.jpeg" data-src="images/c.jpeg" alt="" class="lazy">
+					<img src="img/banners_cat_placeholder.jpg" data-src="img/banner_3.jpg" alt="" class="lazy">
 					<div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
 						<h3>Kids's Collection</h3>
 						<div><span class="btn_1">Shop Now</span></div>
@@ -106,6 +350,12 @@
 				</a>
 			</li>
 		</ul>
+		<!--/banners_grid -->
+	<!-- /header -->
+		
+		
+	<main>
+
 		<!--/banners_grid -->
 		
 		<div class="container margin_60_35">
